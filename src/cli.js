a977551a2914504366fa0f56a035e8e6e687d093
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-05-09 21:14:44 -0300

    feat: criação da função de ler um diretorio com a comparação via fs.lstatSync().isDirectory ou isFile

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,28 @@
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
+import fs from 'fs';
 
 const caminho = process.argv;
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('Lista de links:'), resultado);
+function imprime(resultado){
+    console.log(chalk.blue('O resultado foi:'), resultado);
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        imprime(resultado)
+    }
+    if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (arquivo) => {
+            const resultado = await pegaArquivo(`${caminho}/${arquivo}`)
+            imprime(resultado)
+        })
+    }
+
 }
 
 processaTexto(caminho);
